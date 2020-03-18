6ee0ea0d91fee1f3bc87b1618e6a87fd618ad245
Unknown <sbknick@gmail.com>
2020-03-18 15:29:01 +0000

    removed auth.json in favor of node env vars.

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,5 @@
 import Discord from "discord.js";
 
-import auth from "./auth.json";
-
 import { Bot } from "./Bot";
 import { Convert } from "./Convert";
 import { MessageRouter } from "./Routers/MessageRouter";
@@ -21,4 +19,4 @@ var bot = new Bot(discordClient);
 var messageRouter = new MessageRouter(bot);
 discordClient.on("message", Convert.ToMyMessage().then(messageRouter.route));
 
-discordClient.login(process.env.DISCORD_TOKEN || auth.token);
+discordClient.login(process.env.DISCORD_TOKEN);
